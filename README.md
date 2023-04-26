11d449470280a92f2359a24b07788c84d009a995
Jason Tavera <taverajason7@gmail.com>
2023-04-25 22:04:57 -0500

    backbone code added

---
+++ README.md
@@ -0,0 +1,10 @@
+# Project Description
+  java
+
+
+  ## A second-level heading
+
+  [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+  ### A third-level heading
+
+  
---
+++ index.mjs
@@ -0,0 +1,47 @@
+import inquirer from 'inquirer';
+import fs from "fs/promises";
+
+let {description, size} = await inquirer
+  .prompt([
+    {
+        type: 'input',
+        name: 'description',
+        message: "Write a description of your project",
+      },
+      {
+        type: 'list',
+        name: 'size',
+        message: 'What size do you need?',
+        choices: ['Apache', 'Boost', 'BSD'],
+        filter(val) {
+          return val.toLowerCase();
+        },
+      },
+  ])
+
+  let readmeText = `# Project Description
+  ${description}
+
+
+  ## A second-level heading
+
+  ${generateLicense(size)}
+  ### A third-level heading
+
+  `
+
+  fs.writeFile("README.md", readmeText)
+
+
+  function generateLicense(license){
+    console.log(license);
+    
+    if(license === "apache"){
+
+      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+    }
+
+
+    return ``
+  }
+ 
