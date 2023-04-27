bcc9fd1c2635b3ed57793be7fc0adf9ea840c937
Jason Tavera <taverajason7@gmail.com>
2023-04-26 20:01:26 -0500

    added input types for each section of the readme

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
-# Project Description
-  java
+# java
 
+## Description
+description
 
-  ## A second-level heading
+## Table of Contents
 
-  [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-  ### A third-level heading
+## Installation
+install
 
-  
+## Usage
+instructions
+ 
+## License
+[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+
+## How to Contribute
+contribute
+
+## Tests
+tests
+
+## Questions
+
--- index.mjs
+++ index.mjs
@@ -1,47 +1,83 @@
-import inquirer from 'inquirer';
+import inquirer from "inquirer";
 import fs from "fs/promises";
 
-let {description, size} = await inquirer
-  .prompt([
-    {
-        type: 'input',
-        name: 'description',
-        message: "Write a description of your project",
-      },
-      {
-        type: 'list',
-        name: 'size',
-        message: 'What size do you need?',
-        choices: ['Apache', 'Boost', 'BSD'],
-        filter(val) {
-          return val.toLowerCase();
-        },
-      },
-  ])
+let { title, description, installation, usage, license, contributing, tests } = await inquirer.prompt([
+  
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Write a description of your project:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the steps required to install your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide instructions and examples for use:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Which license would you like for your project?",
+    choices: ["Apache", "Boost", "BSD"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Include guidelines on how you would like other developers to contribute to your project:",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Write tests for your application and provide examples on how to run them here:",
+  },
+]);
 
-  let readmeText = `# Project Description
-  ${description}
+let readmeText = 
 
+`# ${title}
 
-  ## A second-level heading
+## Description
+${description}
 
-  ${generateLicense(size)}
-  ### A third-level heading
+## Table of Contents
 
-  `
-// 
-  fs.writeFile("README.md", readmeText)
+## Installation
+${installation}
 
-
-  function generateLicense(license){
-    console.log(license);
-    
-    if(license === "apache"){
-
-      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
-    }
-
-
-    return ``
-  }
+## Usage
+${usage}
  
+## License
+License the application is covered under: ${generateLicense(license)}
+
+## How to Contribute
+${contributing}
+
+## Tests
+${tests}
+
+## Questions
+
+`;
+
+fs.writeFile("README.md", readmeText);
+
+function generateLicense(license) {
+
+  if (license === "apache") {
+    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else if (license === "Boost") {
+    return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+  } else {
+    return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+  }
+}
